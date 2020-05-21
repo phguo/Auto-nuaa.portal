2d773fb54940da672b290b2d42d8e2cf691f1ea9
guoph <guopenghui668@gmail.com>
2020-05-21 12:08:58 +0800

    update.

--- login.py
+++ login.py
@@ -2,43 +2,86 @@ import sys
 import requests
 import json
 import random
+import socket
 
-username = sys.argv[1]
-password = sys.argv[2]
 
-ip_url = "http://211.65.106.6/drcom/chkstatus?callback=dr1002&v={}".format(random.randint(1000, 9999))
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.connect(("202.119.64.123", 1))
+        ip = s.getsockname()[0]
+    except:
+        ip = "127.0.0.1"
+    finally:
+        s.close()
 
-ip_request = requests.get(ip_url)
-ip_text_content = ip_request.text.replace('dr1002(', '').replace(')', '')
-ip_json_content = json.loads(ip_text_content)
-ip = ip_json_content["v46ip"]
+    if ip == "127.0.0.1":
+        ip_parameters = {
+            "callback": "dr1002",
+            "v": str(random.randint(1000, 9999))
+        }
+        ip_url = "http://211.65.106.6/drcom/chkstatus"
+        ip_request = requests.get(ip_url, params=ip_parameters)
+        ip_text_content = ip_request.text.replace('dr1002(', '').replace(')', '')
+        ip_json_content = json.loads(ip_text_content)
+        ip = ip_json_content["v46ip"]
 
-login_url = "http://211.65.106.6:801/eportal/?c=Portal&a=login&callback=dr1003&login_method=1&user_account=%2C0%2C{}&user_password={}&wlan_user_ip={}&wlan_user_ipv6=&wlan_user_mac=000000000000&wlan_ac_ip=&wlan_ac_name=JiangNing_ME60&jsVersion=3.3.2&v=1060".format(username, password, ip)
+    return ip
 
-login_request = requests.get(login_url)
-login_text_content = login_request.text
-try:
-    login_text_content = login_text_content.replace("dr1003(", '').replace(')', '')
-except:
-    pass
 
-login_json_content = json.loads(login_text_content)
-result = login_json_content.get("result")
-ret_code = login_json_content.get("ret_code")
-msg = login_json_content.get("msg")
+def login(username, password):
+    login_parameters = {
+        "c": "Portal",
+        "a": "login",
+        "callback": "dr1003",
+        "login_method": "1",
+        "user_account": ",0," + username,
+        "user_password": str(password),
+        "wlan_user_ip": get_ip(),
+        "wlan_user_ipv6": "",
+        "wlan_user_mac": "000000000000",
+        "wlan_ac_ip": "",
+        "wlan_ac_name": "JiangNing_ME60",
+        "jsVersion": "3.3.2",
+        "v": "5780",
+    }
+    login_url = "http://211.65.106.6:801/eportal/"
+    login_request = requests.get(login_url, params=login_parameters)
+    login_text_content = login_request.text
+    try:
+        login_text_content = login_text_content.replace("dr1003(", '').replace(')', '')
+    except:
+        pass
 
-if result:
-    result = int(result)
-if ret_code:
-    ret_code = int(ret_code)
+    login_json_content = json.loads(login_text_content)
+    result = login_json_content.get("result")
+    ret_code = login_json_content.get("ret_code")
+    msg = login_json_content.get("msg")
 
-if result == 1 and msg == "\u8ba4\u8bc1\u6210\u529f":  # 认证成功
-    query = "✅ \"{}\" Logged in successfully!".format(username)
-elif result == 0 and msg == "" and ret_code == 2:
-    query = "😑 \"{}\" has already logged in.".format(username)
-elif result == 0 and ret_code == 1:
-    query = "🚫 Wrong password for \"{}\".".format(username)
-else:
-    query = "❓ Unknown error for \"{}\".".format(username)
+    if result:
+        result = int(result)
+    if ret_code:
+        ret_code = int(ret_code)
 
-sys.stdout.write(query)
+    if result == 1 and msg == "\u8ba4\u8bc1\u6210\u529f":  # 认证成功
+        query = "✅ \"{}\" Logged in successfully!".format(username)
+    elif result == 0 and msg == "" and ret_code == 2:
+        query = "😑 \"{}\" has already logged in.".format(username)
+    elif result == 0 and ret_code == 1:
+        query = "🚫 Wrong password for \"{}\".".format(username)
+    else:
+        query = "❓ Unknown error for \"{}\".".format(username)
+
+    return query
+
+
+if __name__ == '__main__':
+    try:
+        from config import U, P
+        query = login(U, P)
+    except:
+        try:
+            query = login(sys.argv[1], sys.argv[2])
+        except:
+            query = "Unknown Error for Login!"
+    sys.stdout.write(query)
--- logout.py
+++ logout.py
@@ -1,28 +1,57 @@
 import sys
 import requests
 import json
+from login import get_ip
 
-logout_url = "http://211.65.106.6:801/eportal/?c=Portal&a=logout&callback=dr1006&login_method=1&user_account=drcom&user_password=123&ac_logout=1&register_mode=1&wlan_user_ip=172.31.212.243&wlan_user_ipv6=&wlan_vlan_id=0&wlan_user_mac=4c32758cf9a5&wlan_ac_ip=&wlan_ac_name=&jsVersion=3.3.2&v=8914"
 
-logout_request = requests.get(logout_url)
-logout_text_content = logout_request.text
-try:
-    logout_text_content = logout_text_content.replace("dr1006(", '').replace(')', '')
-except:
-    pass
+def logout():
+    parameters = {
+        "c": "Portal",
+        "a": "logout",
+        "callback": "dr1006",
+        "login_method": "1",
+        "user_account": "drcom",
+        "user_password": "123",
+        "ac_logout": "1",
+        "register_mode": "1",
+        "wlan_user_ip": get_ip(),
+        "wlan_user_ipv6": "",
+        "wlan_vlan_id": "",
+        "wlan_user_mac": "4c32758cf9a5",
+        "wlan_ac_ip": "",
+        "wlan_ac_name": "",
+        "jsVersion": "3.3.2",
+        "v": "8914"
+    }
+    logout_url = "http://211.65.106.6:801/eportal/"
 
-logout_json_content = json.loads(logout_text_content)
-result = logout_json_content.get("result")
-msg = logout_json_content.get("msg")
+    logout_request = requests.get(logout_url, params=parameters)
+    logout_text_content = logout_request.text
+    try:
+        logout_text_content = logout_text_content.replace("dr1006(", '').replace(')', '')
+    except:
+        pass
 
-if result:
-    result = int(result)
+    logout_json_content = json.loads(logout_text_content)
+    result = logout_json_content.get("result")
+    msg = logout_json_content.get("msg")
 
-if result == 1 and msg == "\u6ce8\u9500\u6210\u529f":  # 注销成功
-    query = "Logged out successfully!"
-elif result == 0 and msg == "\u6ce8\u9500\u5931\u8d25":  # 注销失败
-    query = "Log out failed (Not logged in yet)."
-else:
-    query = "Unknown error."
+    if result:
+        result = int(result)
 
-sys.stdout.write(query)
+    if result == 1 and msg == "\u6ce8\u9500\u6210\u529f":  # 注销成功
+        query = "Logged out successfully!"
+    elif result == 0 and msg == "\u6ce8\u9500\u5931\u8d25":  # 注销失败
+        query = "Log out failed (Not logged in yet)."
+    else:
+        query = "Unknown error."
+
+    return query
+
+
+if __name__ == "__main__":
+    try:
+        query = logout()
+    except:
+        query = "Unknown Error for Log out!"
+    sys.stdout.write(query)
